e6c5883774a0873f69c91f8f0c58c3985644c041
Tomasz Pietraszek <tomekpietraszek@gmail.com>
2024-01-24 12:27:22 +0100

    cors origin url added

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 const { PORT = 5000, DB_URI } = process.env;
 
 app
-  .use(cors({ origin: "http://localhost:3000", credentials: true }))
+  .use(cors({ origin: ["http://localhost:3000", "https://record-shop-tomek.onrender.com"], credentials: true }))
   .use(express.json())
   .use(cookieParser())
   .use("/records", recordsRouter)
